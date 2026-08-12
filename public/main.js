7372d05bae2b4dffcbc10437caece69ac11d79d7
index-daddy372466 <testmailbox987654@gmail.com>
2026-08-12 15:45:36 -0400

    chore: update accounts and merge

--- public/main.js
+++ public/main.js
@@ -1,8 +1,27 @@
 // variables
 const logoContainer = document.getElementById('logo-container');
 const email_input = document.getElementById('email-input');
-const newsletterForm = document.getElementById('newsletter-form')
+const newsletterForm = document.getElementById('newsletter-form');
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const hamburger_menu = document.getElementById('hamburger-menu');
+const hamburger_spans = [...hamburger_menu.children];
+const span_rotate = 'span-rotate'
+const hamburger_opens = ['top','mid','bottom']
+
+
+
 // functions
+// toggle hamburger with children & value (true/false)
+function toggleHamburgerMenu(children) {
+    for(let i = 0; i < children.length; i++) {
+        // console.log(children[i])
+        // console.log(children.indexOf(children[i]))
+
+        // toggle each span
+        children[i].classList.toggle(`${span_rotate}-${hamburger_opens[i]}`)
+    }
+}
+toggleHamburgerMenu(hamburger_spans)
 function handleWindowScroll(e) {
     const {scrollY} = window;
     const isPast = (y) => y > 0;
@@ -15,21 +34,10 @@ function handleWindowScroll(e) {
         logoContainer.classList.remove('fixed-logo');
     }
 }
-
 // onscroll event
 window.onscroll = handleWindowScroll
 
-
-export function vibrateMode() {
-    if(navigator.vibrate) {
-        navigator.vibrate(133);
-
-        console.log('vibration success')
-        // alert('vibration success')
-    }
-}
-
-let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+// verify email
 function verifyEmail(value) {
     return emailRegex.test(value);
 }
@@ -38,6 +46,7 @@ if(email_input){
     email_input.oninput = handleEmailInput
 }
 
+// handle email input
 function handleEmailInput(e) {
     const target = e.target;
     const value = e.target.value;
@@ -55,8 +64,18 @@ function handleEmailInput(e) {
     }
 } 
 
-
+// handle submit
 function handleSubmit(e) {
     e.preventDefault()
     console.log(e)
+}
+
+// virbate mode
+export function vibrateMode() {
+    if(navigator.vibrate) {
+        navigator.vibrate(133);
+
+        console.log('vibration success')
+        // alert('vibration success')
+    }
 }
